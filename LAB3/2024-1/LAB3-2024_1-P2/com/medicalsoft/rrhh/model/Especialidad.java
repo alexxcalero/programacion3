5e674984fe3a58c5828709f2aec002b25700fb7e
Alex Calero Revilla <alexcalerorevilla@gmail.com>
2024-12-22 17:16:46 -0500

    Desarrollo de la pregunta 2 del laboratorio 3 del ciclo 2024-1

---
+++ LAB3/2024-1/LAB3-2024_1-P2/com/medicalsoft/rrhh/model/Especialidad.java
@@ -0,0 +1,13 @@
+package com.medicalsoft.rrhh.model;
+
+public class Especialidad {
+	private String nombre;
+	
+	public Especialidad(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+}
