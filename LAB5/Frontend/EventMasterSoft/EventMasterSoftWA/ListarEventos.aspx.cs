6f0dc2b275302cf870d5f0a235b26a9cf17c55d5
Alex Calero Revilla <alexcalerorevilla@gmail.com>
2024-12-25 02:15:41 -0500

    Desarrollo del frontend del laboratorio 5 del ciclo 2024-2

---
+++ LAB5/Frontend/EventMasterSoft/EventMasterSoftWA/ListarEventos.aspx.cs
@@ -0,0 +1,39 @@
+﻿/* Colocar sus datos personales
+ * ------------------------------------------------
+ * Nombre Completo: Alex Calero Revilla
+ * Codigo PUCP: 20206455
+ * ------------------------------------------------
+ */
+
+using EventMasterSoftWA.ServicioWS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EventMasterSoft
+{
+    public partial class ListarEventos : System.Web.UI.Page
+    {
+        private EventoWSClient daoEvento = new EventoWSClient();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            gvEventos.DataSource = daoEvento.listarEventosPorNombre(txtNombre.Text);
+            gvEventos.DataBind();
+        }
+
+        protected void lbRegistrar_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("RegistrarEvento.aspx");
+        }
+
+        protected void lbVisualizar_Click(object sender, EventArgs e)
+        {
+            int idEvento = Int32.Parse(((LinkButton)sender).CommandArgument);
+            Response.Redirect("RegistrarEvento.aspx?accion=ver&idEvento=" + idEvento);
+        }
+    }
+}
